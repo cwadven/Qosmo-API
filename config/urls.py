f2b948bb78eb08e95e310e389764a9ca114d475b
cwadven <cwadven@naver.com>
2024-12-27 17:43:41 +0900

    Feat: Add yaml swagger
    - Add yaml swagger

--- config/urls.py
+++ config/urls.py
@@ -27,3 +27,44 @@ if settings.DEBUG:
     urlpatterns += [
         path('__debug__/', include(debug_toolbar.urls)),
     ]
+
+
+if settings.DJANGO_SETTINGS_MODULE != 'config.settings.production':
+    import yaml
+    from rest_framework import permissions
+    from drf_yasg.views import get_schema_view
+    from drf_yasg import openapi
+
+    # swagger.yaml 파일 로드
+    with open('docs/Swagger/swagger.yaml', 'r', encoding='utf-8') as f:
+        swagger_yaml = yaml.safe_load(f)
+
+    info = swagger_yaml.get('info', {})
+    schema_view = get_schema_view(
+        openapi.Info(
+            title=info.get('title', 'Checker API'),
+            default_version=info.get('version', 'v1'),
+            description=info.get('description', ''),
+        ),
+        url=swagger_yaml.get('servers', [{}])[0].get('url', ''),
+        patterns=None,
+        public=True,
+        permission_classes=[permissions.AllowAny],
+        generator_class=None,
+    )
+
+    urlpatterns += [
+        path(
+            'swagger/',
+            schema_view.with_ui('swagger', cache_timeout=0),
+            name='schema-swagger-ui',
+        ),
+        path(
+            'swagger.yaml',
+            TemplateView.as_view(
+                template_name='swagger/swagger.yaml',
+                content_type='text/yaml',
+            ),
+            name='schema-yaml',
+        ),
+    ]
