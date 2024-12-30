d66edbd74c33c9774d1972a28be9f0e06dd121f4
cwadven <cwadven@naver.com>
2024-12-30 14:24:56 +0900

    Feat: Create subscription App
    - Create subscription App

--- subscription/admin.py
+++ subscription/admin.py
@@ -1,3 +1,39 @@
 from django.contrib import admin
+from subscription.models import MapSubscription
 
-# Register your models here.
+
+@admin.register(MapSubscription)
+class MapSubscriptionAdmin(admin.ModelAdmin):
+    list_display = (
+        'id',
+        'member',
+        'map',
+        'is_deleted',
+        'created_at',
+        'updated_at',
+    )
+    list_filter = (
+        'is_deleted',
+        'created_at',
+        'updated_at',
+    )
+    search_fields = (
+        'member__nickname',
+        'member__username',
+        'map__name',
+    )
+    raw_id_fields = (
+        'member',
+        'map',
+    )
+    readonly_fields = (
+        'created_at',
+        'updated_at',
+    )
+    ordering = ('-created_at',)
+
+    def get_queryset(self, request):
+        return super().get_queryset(request).select_related(
+            'member',
+            'map',
+        )
--- subscription/models.py
+++ subscription/models.py
@@ -1,3 +1,32 @@
 from django.db import models
+from map.models import Map
+from member.models import Member
 
-# Create your models here.
+
+class MapSubscription(models.Model):
+    member = models.ForeignKey(
+        Member,
+        on_delete=models.DO_NOTHING,
+        related_name='map_subscriptions',
+        help_text='구독한 사용자'
+    )
+    map = models.ForeignKey(
+        Map,
+        on_delete=models.DO_NOTHING,
+        related_name='subscriptions',
+        help_text='구독한 Map'
+    )
+    is_deleted = models.BooleanField(default=False)
+    created_at = models.DateTimeField(
+        auto_now_add=True,
+        help_text='구독 시작 시각',
+        db_index=True
+    )
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        verbose_name = 'Map 구독'
+        verbose_name_plural = 'Map 구독'
+
+    def __str__(self):
+        return f'{self.member.nickname}의 {self.map.name} 구독'
