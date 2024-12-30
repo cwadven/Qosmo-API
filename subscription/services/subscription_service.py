12cb2c0590c273863e351fc3e91bef0ca72e71f5
cwadven <cwadven@naver.com>
2024-12-30 14:31:29 +0900

    Feat: Get Service for subscription
    - Get Service for subscription

---
+++ subscription/services/subscription_service.py
@@ -0,0 +1,30 @@
+from typing import (
+    Dict,
+    List,
+    Optional,
+)
+
+from subscription.models import MapSubscription
+
+
+class MapSubscriptionService:
+    def __init__(self, member_id: Optional[int] = None):
+        self.member_id = member_id
+
+    def get_subscription_status_by_map_ids(self, map_ids: List[int]) -> Dict[int, bool]:
+        if not self.member_id:
+            return {map_id: False for map_id in map_ids}
+
+        subscriptions = MapSubscription.objects.filter(
+            member_id=self.member_id,
+            map_id__in=map_ids,
+            is_deleted=False
+        ).values_list(
+            'map_id',
+            flat=True,
+        )
+        subscription_status = {
+            map_id: (map_id in subscriptions)
+            for map_id in map_ids
+        }
+        return subscription_status
