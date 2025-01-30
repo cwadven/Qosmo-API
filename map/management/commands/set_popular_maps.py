594d05fd27c46256f5055ad4c201e6dbad519d95
이창우(Changwoo Lee) <changwoo.lee@wesang.com>
2025-01-30 10:24:04 +0900

    Feat: Add command for popular
    - Add command for popular

---
+++ map/management/commands/set_popular_maps.py
@@ -0,0 +1,74 @@
+from datetime import datetime
+from typing import Dict
+
+from django.core.management.base import BaseCommand
+from django.db.models import Count, QuerySet
+
+from map.models import PopularMap, Map
+from subscription.models import MapSubscription
+
+
+class Command(BaseCommand):
+    help = '인기 맵 데이터를 최신화 합니다.'
+
+    def add_arguments(self, parser):
+        parser.add_argument('type', type=str, help='daily, monthly')
+
+    def _get_popular_map_subscription_qs(self) -> QuerySet[Dict]:
+        if self.type == 'daily':
+            queryset = MapSubscription.objects.filter(
+                is_deleted=False,
+                map__is_deleted=False,
+                map__is_private=False,
+                updated_at__date=datetime.now().date()
+            ).values('map_id').annotate(
+                subscriber_count=Count('member_id', distinct=True)
+            ).order_by(
+                '-subscriber_count'
+            )
+        elif self.type == 'monthly':
+            queryset = MapSubscription.objects.filter(
+                is_deleted=False,
+                map__is_deleted=False,
+                map__is_private=False,
+                updated_at__month=datetime.now().month
+            ).values('map_id').annotate(
+                subscriber_count=Count('member_id', distinct=True)
+            ).order_by(
+                '-subscriber_count'
+            )
+        else:
+            queryset = MapSubscription.objects.none()
+        return queryset
+
+    def handle(self, *args, **options):
+        self.type = options['type']
+        if self.type not in ['daily', 'monthly']:
+            self.stdout.write(self.style.ERROR('type은 daily 또는 monthly만 가능합니다.'))
+            return
+
+        popular_map_subscription_qs = self._get_popular_map_subscription_qs()[:10]
+        map_by_map_id = {
+            _map.id: _map
+            for _map in Map.objects.filter(
+                id__in=[
+                    popular_map_subscription['map_id']
+                    for popular_map_subscription in popular_map_subscription_qs
+                ]
+            )
+        }
+        PopularMap.objects.filter(
+            type=self.type,
+            is_deleted=False,
+        ).update(
+            is_deleted=True
+        )
+        PopularMap.objects.bulk_create([
+            PopularMap(
+                map=map_by_map_id[popular_map_subscription['map_id']],
+                type=self.type,
+                subscriber_count=popular_map_subscription['subscriber_count'],
+                view_count=0,
+            )
+            for popular_map_subscription in popular_map_subscription_qs
+        ])
