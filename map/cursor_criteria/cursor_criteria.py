c18579377402100d148b761822d15c7477b9387f
이창우(Changwoo Lee) <changwoo.lee@wesang.com>
2025-02-02 09:19:46 +0900

    Feat: Create My Map list
    - Create My Map list

--- map/cursor_criteria/cursor_criteria.py
+++ map/cursor_criteria/cursor_criteria.py
@@ -4,7 +4,6 @@ from common.common_criteria.cursor_criteria import CursorCriteria
 class MapListCursorCriteria(CursorCriteria):
     cursor_keys = [
         'id__lt',
-        'created_at__lte',
     ]
 
 
