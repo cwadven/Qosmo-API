3c469be6b05d31f37b85b646f5c6256eef9553bc
이창우(Changwoo Lee) <changwoo.lee@wesang.com>
2025-02-19 21:17:58 +0900

    Feat: Add admin
    - Add admin

--- push/admin.py
+++ push/admin.py
@@ -1,3 +1,35 @@
 from django.contrib import admin
+from push.models import (
+    DeviceToken,
+    PushHistory,
+)
 
-# Register your models here.
+
+@admin.register(DeviceToken)
+class DeviceTokenAdmin(admin.ModelAdmin):
+    list_display = (
+        'id',
+        'guest',
+        'token',
+        'device_type',
+        'is_active',
+        'created_at',
+        'updated_at',
+    )
+    readonly_fields = ('created_at', 'updated_at')
+
+
+@admin.register(PushHistory)
+class PushHistoryAdmin(admin.ModelAdmin):
+    list_display = (
+        'id',
+        'guest',
+        'device_token',
+        'title',
+        'body',
+        'data',
+        'sent_at',
+        'is_success',
+        'error_message',
+    )
+    readonly_fields = ('sent_at',)
