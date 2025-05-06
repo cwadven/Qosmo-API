569e9e6e6605251acc56ce7a6eebbdc42fffe6f9
이창우(Changwoo Lee) <changwoo.lee@wesang.com>
2025-05-06 10:08:28 +0900

    Feat: Set reminder cron
    - Set reminder cron

--- push/consts.py
+++ push/consts.py
@@ -6,6 +6,7 @@ class PushChannelType(StrValueLabel):
     QUESTION_FEEDBACK = 'question_feedback', '문제 피드백'
     QUESTION_SOLVED_ALERT = 'question_solved_alert', '문제 해결 알림'
     ROLE_CHANGE = 'role_change', '역할 변경'
+    MAP_PLAY_MEMBER_REMINDER = 'map_play_member_reminder', '맵 플레이 멤버 리마인더'
 
 
 class PushMapPlayMemberPushType(StrValueLabel):
--- push/management/commands/push_map_play_member_reminder.py
+++ push/management/commands/push_map_play_member_reminder.py
@@ -1,77 +1,19 @@
-from typing import Dict
-
 from django.core.management.base import BaseCommand
-from django.db.models import (
-    Count,
-    QuerySet,
-)
-from django.utils import timezone
 
-from map.models import (
-    Map,
-    PopularMap,
-)
-from play.models import MapPlay
 from push.consts import PushChannelType
 from push.models import PushMapPlayMember
 from push.services import PushService
-from subscription.models import MapSubscription
+
+
+PUSH_REMIND_TITLE = "리마인드 알림"
+PUSH_REMIND_BODY = "{} 의 {} 리마인드 알림 입니다."
 
 
 class Command(BaseCommand):
     """
     python manage.py push_map_play_member_reminder
     """
-    help = '알림을 통해 해야할 MapPlayMember 를 통한 리마인드, 리마인더.'\
-
-    def _get_popular_map_subscription_qs(self) -> QuerySet[Dict]:
-        now = timezone.now()
-        queryset = MapSubscription.objects.filter(
-            is_deleted=False,
-            map__is_deleted=False,
-            map__is_private=False,
-        )
-        if self.type == 'daily':
-            queryset = queryset.filter(
-                updated_at__date=now.date()
-            )
-        elif self.type == 'monthly':
-            queryset = queryset.filter(
-                updated_at__gte=now.replace(day=1),
-                updated_at__lt=now.replace(day=1, month=now.month+1)
-            )
-        else:
-            queryset = MapSubscription.objects.none()
-        queryset = queryset.values('map_id').annotate(
-            subscriber_count=Count('member_id', distinct=True)
-        ).order_by(
-            '-subscriber_count'
-        )
-        return queryset
-
-    def _get_popular_map_play_qs(self) -> QuerySet[Dict]:
-        now = timezone.now()
-        queryset = MapPlay.objects.filter(
-            map__is_deleted=False,
-            map__is_private=False,
-        )
-        if self.type == 'daily':
-            queryset = queryset.filter(
-                created_at__date=now.date()
-            )
-        elif self.type == 'monthly':
-            queryset = queryset.filter(
-                created_at__gte=now.replace(day=1),
-                created_at__lt=now.replace(day=1, month=now.month + 1)
-            )
-        else:
-            queryset = MapPlay.objects.none()
-        queryset = queryset.values('map_id').annotate(
-            play_count=Count('created_by_id', distinct=True)
-        ).order_by(
-            '-play_count'
-        )
-        return queryset
+    help = '알림을 통해 해야할 MapPlayMember 를 통한 리마인드, 리마인더.'
 
     def handle(self, *args, **options):
         push_map_play_members = PushMapPlayMember.objects.select_related(
@@ -86,10 +28,10 @@ class Command(BaseCommand):
             map_play_title = push_map_play_member.map_play_member.map_play.title
             push_service.send_push(
                 guest_id=push_map_play_member.guest_id,
-                title=f"Qosmo 리마인드 알림 입니다.",
-                body=f"{map_name} 의 {map_play_title} 리마인드 알림 입니다.",
+                title=PUSH_REMIND_TITLE,
+                body=PUSH_REMIND_BODY.format(map_name, map_play_title),
                 data={
-                    "type": "map_play_member_reminder",
+                    "type": PushChannelType.MAP_PLAY_MEMBER_REMINDER.value,
                     "map_id": str(push_map_play_member.map_play_member.map_play.map_id),
                 },
             )
